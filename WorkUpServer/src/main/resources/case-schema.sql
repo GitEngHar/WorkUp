91e8372b109da2c539e54fde04f04906e9901758
GitEngHar <harukii1999@gmail.com>
2025-03-15 19:13:01 +0900

    case sql

--- WorkUpServer/src/main/resources/case-schema.sql
+++ WorkUpServer/src/main/resources/case-schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS house (
     house_id INTEGER NOT NULL PRIMARY KEY,
     region CHAR(32) NOT NULL
 );
+
+-- city case テスト用
+CREATE TABLE IF NOT EXISTS city (
+    population INTEGER NOT NULL,
+    city CHAR(32) NOT NULL,
+    CONSTRAINT pk_City PRIMARY KEY (city)
+);
